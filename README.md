41837a9243044976d4ee952e5360eeccb23da3ad
Randy King <randy.king@kount.com>
2021-04-21 06:57:47 -0600

    Add README

---
+++ README.md
@@ -0,0 +1,1 @@
+Run `./basic-linux-node-server/init.sh blue` or `./basic-linux-node-server/init.sh green` from ec2-user home directory to initialize the "blue" or "green" server, respectively.
--- init.sh
+++ init.sh
@@ -9,24 +9,5 @@ sudo yum update -y
 sudo yum install -y git
 git clone https://github.com/brandallk/basic-linux-node-server.git
 
-# install nodejs & npm
-sudo curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-sudo yum install -y nodejs
-
-# install express
-mkdir node-server && pushd node-server
-npm init -y
-npm install express --save-dev
-popd
-
-# copy in the index.js file for "blue" or "green" server
-cp basic-linux-node-server/index-$NODE_SERVER.js node-server/index-$NODE_SERVER.js
-
-# copy the systemd unit file for "blue" or "green" server
-sudo cp basic-linux-node-server/$NODE_SERVER.service /lib/systemd/system/$NODE_SERVER.service
-
-# ensure server restart
-sudo systemctl enable $NODE_SERVER
-
-# start the server
-sudo systemctl start $NODE_SERVER
+# run server init for "blue" or "green"
+./basic-linux-node-server/setup.sh $NODE_SERVER
