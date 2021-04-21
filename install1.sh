3587b25b1cda0633ea5ac64d21a1d064380b1565
Randy King <randy.king@kount.com>
2021-04-21 06:20:47 -0600

    update init script

--- install1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum update -y
-sudo curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
-sudo yum install -y nodejs
-
-mkdir node-server && cd node-server
-npm init -y
-npm install express --save-dev
--- install2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir node-server
-cp basic-linux-node-server/index.js node-server/index.js
-
-cp basic-linux-node-server/blue.service /lib/systemd/system/blue.service
